936383fef675796d5362797f9f9b82c0f1004499
fiza0 <issafuad05@gmail.com>
2025-07-01 22:50:25 +0300

    create and link about footer and project components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,20 @@
-import Header from './components/Header'
+import Header from './components/Header';
+import Body from './components/Body';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
   return (
     <div id="app" className="App">
-        <Header />
+        <Header/>
+        <Body/>
+        <About/>
+        <Projects/>
+        <Skills/>
+        <Footer/>
     </div>
   );
 }
