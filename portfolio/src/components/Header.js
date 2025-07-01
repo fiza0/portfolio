af15872e63f975183bea05c14e18039fd7db5dee
fiza0 <issafuad05@gmail.com>
2025-07-01 21:30:58 +0300

    create header component

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,1 +1,17 @@
 // Create your Header component here
+const Header = () =>{
+    return (
+        <div className="header">
+            <div className="header-logo">
+                <a href="#app"><h3 className="header-name">Fuad Issa</h3></a>
+            </div>
+            <div className="header-container">
+                <div className="nav-item"><a href="#about">about</a></div>
+                <div className="nav-item"><a href="#projects">projects</a></div>
+                <div className="nav-item"><a href="#skills">skills</a></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
